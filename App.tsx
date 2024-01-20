6b2b17fcbd625cdeac933308a04e9942a698d1cf
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-01-21 01:29:00 +0400

    init

--- App.tsx
+++ App.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 
-import SQLite from 'react-native-sqlite-storage';
 import {Text, View} from 'react-native';
-
-const db = SQLite.openDatabase(
-  {
-    name: 'Main',
-    location: 'default',
-  },
-  () => {},
-  error => console.log(error),
-);
 
 function App(): React.JSX.Element {
   return (
