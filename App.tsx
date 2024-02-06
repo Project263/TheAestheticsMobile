9271a07eb94d1ab93ae4f61cf3177f8e414912c9
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-02-06 18:40:49 +0400

    feat(TA-1): добавил Layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, View } from "react-native";
 import { Header } from "./src/components";
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Header />
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <Header />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-  },
-});
