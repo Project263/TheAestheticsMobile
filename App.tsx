9b3e740cd7d63f544843751c2a4d95db4d338853
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-02-29 18:26:25 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,14 +4,7 @@ import { palette, primary } from "src/constants/colors";
 export default function App() {
   return (
     <SafeAreaView>
-      <ScrollView>
-        <Text style={{ color: primary[0] }}>text</Text>
-        <Text style={{ color: primary[50] }}>text</Text>
-        <Text style={{ color: palette.primary }}>text</Text>
-        {/* {Object.entries(colorMap).map(([key, value]) => (
-          <Text style={{ color: value }}>text</Text>
-        ))} */}
-      </ScrollView>
+    
     </SafeAreaView>
   );
 }
